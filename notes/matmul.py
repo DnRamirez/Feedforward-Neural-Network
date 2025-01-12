48a25157c14790d88f961739178d2279dfdeea93
Danster23 <ramirezdanny02@gmail.com>
2025-01-12 15:34:45 -0600

    notes

--- notes/matmul.py
+++ notes/matmul.py
@@ -1,34 +1,23 @@
+# Matrix multiplication is a fundamental operation when working with neural networks 
+# A matrix is a 2D array of numbers consisting of rows and columns
+# A matrix with 2 rows and 3 columns is a 2x3 matrix
+# GPUs are best suited for matrix multiplication because they can perform many calculations in parallel
 import numpy as np 
-
 op1 = np.array([[1, 2, 3],
                 [1, 4, 5]])
 
-print(op1.shape)
+print(f"Shape of first matrix is: {op1.shape}")
 
 op2 = np.array([[1],
                 [2],
                 [3]])
 
-print(op2.shape) # should output 3,1 
+print(f"Shape of second matrix is: {op2.shape}") 
 
-# now you can multiply these because the inner dimensions match
+# You can multiply these matrices because the inner dimensions match (3 == 3)
 dotproduct = np.matmul(op1, op2)
-print(dotproduct)
-
-op3 = np.array([[1, 2],
-                [4, 5],
-                [7, 8]])
-
-print(op3.shape)
-
-op4 = np.array([[3],
-                [4]])
-
-print(op4.shape)
-
-product = np.matmul(op3, op4)
-print("product shape is: ", product.shape)
-print("product result is: ", product)
+print("product shape is: ", dotproduct.shape)
+print("product result is: ", dotproduct)
 
 
 
